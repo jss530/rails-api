d35f928a574e94bdbb913fe30ff07fd1f109b342
jss530 <jsifre01@gmail.com>
2018-12-15 17:29:10 -0500

    got first set of tests passing and created initial controllers

--- Todo/app/models/item.rb
+++ Todo/app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :todo
+  belongs_to :list
+
+  validates_presence_of :name
 end
--- Todo/app/models/list.rb
+++ Todo/app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ApplicationRecord
+  has_many :items, dependent: :destroy
+
+  validates_presence_of :title, :created_by
 end
